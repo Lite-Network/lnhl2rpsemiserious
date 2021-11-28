a94bc5757a0ea3f357fa9d059a02969e7e8922d2
Riggs <49407096+RiggsMackay@users.noreply.github.com>
2021-11-28 21:29:00 +0100

    Plugins

---
+++ plugins/update_griddivision/sh_plugin.lua
@@ -0,0 +1,5 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "Grid Division Update"
+PLUGIN.author = "Riggs Mackay"
+PLUGIN.description = "Adds Features and improvements to the Grid Division in Lite Network."
---
+++ plugins/update_jurydivision/sh_plugin.lua
@@ -0,0 +1,5 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "Jury Division Update"
+PLUGIN.author = "Riggs Mackay"
+PLUGIN.description = "Adds Features and improvements to the Jury Division in Lite Network."
