fd8dd8d62e2f51bb5afd56bd955f4cfd5726bd1a
Riggs <49407096+RiggsMackay@users.noreply.github.com>
2021-11-28 21:28:51 +0100

    Started work on Rebel Update

---
+++ plugins/update_rebel/items/sh_ied.lua
@@ -0,0 +1,71 @@
+-- Item Statistics
+
+ITEM.name = "IED"
+ITEM.description = "A Bomb filled with explosives and a timer."
+ITEM.category = "Weapons"
+
+-- Item Configuration
+
+ITEM.model = "models/weapons/w_c4_planted.mdl"
+ITEM.skin = 0
+ITEM.price = 3700
+
+local function PlayBeep(time, ent)
+	timer.Simple(time or 0, function()
+		if ent:IsValid() then
+			ent:EmitSound("weapons/c4/c4_click.wav", 90)
+		end
+	end)
+end
+
+ITEM.functions.PlaceAPC = {
+	name = "Place on APC",
+	OnCanRun = function(item)
+		local ply = item.player
+		local data = {}
+			data.start = ply:GetShootPos()
+			data.endpos = data.start + ply:GetAimVector() * 96
+			data.filter = ply
+		local ent = util.TraceLine(data).Entity
+
+		if ( ent:IsValid() ) and ( ent:GetClass() == "gmod_sent_vehicle_fphysics_base" ) and ( ent:GetModel():find("combine_apc") ) then
+			return true
+		else
+			return false
+		end
+	end,
+	OnRun = function(item)
+		local ply = item.player
+		local data = {}
+			data.start = ply:GetShootPos()
+			data.endpos = data.start + ply:GetAimVector() * 96
+			data.filter = ply
+		local ent = util.TraceLine(data).Entity
+
+		if ( ent:IsValid() ) and ( ent:GetClass() == "gmod_sent_vehicle_fphysics_base" ) and ( ent:GetModel():find("combine_apc") ) then
+			ent:EmitSound("weapons/c4/c4_plant.wav", 90)
+			PlayBeep(0.5, ent)
+			PlayBeep(1.5, ent)
+			PlayBeep(2.5, ent)
+			PlayBeep(3, ent)
+			PlayBeep(3.25, ent)
+			PlayBeep(3.50, ent)
+			PlayBeep(3.75, ent)
+			timer.Simple(4, function()
+				if ent:IsValid() then
+					local explode = ents.Create("env_explosion")
+					explode:SetPos(ent:GetPos())
+					explode:SetOwner(ply)
+					explode:Spawn()
+					explode:SetKeyValue("iMagnitude", "375")
+					explode:Fire("Explode", 0, 0)
+					explode:EmitSound("weapons/c4/c4_explode1.wav", 120)
+					explode:EmitSound("weapons/c4/c4_exp_deb1.wav", 100)
+					explode:EmitSound("weapons/c4/c4_exp_deb2.wav", 100)
+				end
+			end)
+		end
+
+		return true
+	end
+}
---
+++ plugins/update_rebel/sh_plugin.lua
@@ -0,0 +1,5 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "Resistance Update"
+PLUGIN.author = "Riggs Mackay"
+PLUGIN.description = "Adds Features and improvements to the Resistance in Lite Network."
