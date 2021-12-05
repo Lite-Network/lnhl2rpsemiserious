53f5a72c30b47481f1cbec30d9f1f6bd062ebb3b
Riggs <49407096+RiggsMackay@users.noreply.github.com>
2021-12-05 14:06:37 +0100

    Intro Plugin Work

--- plugins/intro/sh_plugin.lua
+++ plugins/intro/sh_plugin.lua
@@ -3,3 +3,11 @@ local PLUGIN = PLUGIN
 PLUGIN.name = "Lite Network Intro"
 PLUGIN.author = "Riggs Mackay"
 PLUGIN.description = "A Intro that loads after creating your character."
+
+ix.util.Include("sv_plugin.lua")
+
+if ( CLIENT ) then
+    net.Receive("ixStartIntro", function(len, ply)
+        
+    end)
+end
---
+++ plugins/intro/sv_plugin.lua
@@ -0,0 +1,8 @@
+function PLUGIN:StartIntro(ply)
+    net.Start("ixStartIntro")
+    net.Send(ply)
+end
+
+function PLUGIN:PlayerLoadedCharacter(ply, char, lastChar)
+    PLUGIN:StartIntro(ply)
+end
