79031f41ba4c5cbf496b42f85450139f63227729
Riggs <49407096+RiggsMackay@users.noreply.github.com>
2021-12-07 23:24:42 +0100

    New Scoreboard

---
+++ schema/derma/cl_scoreboard.lua
@@ -0,0 +1,59 @@
+local PANEL = {}
+
+function PANEL:Init()
+	self:Dock(FILL)
+
+    local players = player.GetAll()
+    table.sort(players, function(k, v) return k:Team() > v:Team() end)
+
+    for k, v in pairs(players) do
+        local playerBox = self:Add("DPanel")
+        playerBox:SetSize(self:GetWide(), 70)
+        playerBox:Dock(TOP)
+        playerBox:DockMargin(0, 0, 0, 5)
+        playerBox.Paint = function(this)
+            draw.RoundedBox(5, 0, 0, playerBox:GetWide(), playerBox:GetTall(), ColorAlpha(team.GetColor(v:Team()), 100))
+
+            draw.SimpleText(v:SteamName(), "LiteNetworkFont32", 80, playerBox:GetTall() / 2, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+            draw.SimpleText(team.GetName(v:Team()), "LiteNetworkFont32", playerBox:GetWide() / 2, playerBox:GetTall() / 2, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+        end
+
+        local playerIcon = playerBox:Add("ixSpawnIcon")
+        playerIcon:SetSize(playerBox:GetTall() - 10, playerBox:GetTall() - 10)
+        playerIcon:Dock(LEFT)
+        playerIcon:DockMargin(5, 5, 5, 5)
+        playerIcon:SetModel(v:GetModel(), v:GetSkin())
+        playerIcon.DoRightClick = function()
+            local ply = self.player
+    
+            if (!IsValid(ply)) then
+                return
+            end
+    
+            local menu = DermaMenu()
+    
+            menu:AddOption(L("viewProfile"), function()
+                ply:ShowProfile()
+            end)
+    
+            menu:AddOption(L("copySteamID"), function()
+                SetClipboardText(ply:IsBot() and ply:EntIndex() or ply:SteamID())
+            end)
+    
+            hook.Run("PopulateScoreboardPlayerMenu", ply, menu)
+            menu:Open()
+        end
+
+        for k2, v2 in pairs(v:GetBodyGroups()) do
+            playerIcon.Entity:SetBodygroup(v2.id, v:GetBodygroup(v2.id))
+        end
+    end
+end
+
+vgui.Register("ixScoreboard", PANEL, "DScrollPanel")
+
+hook.Add("CreateMenuButtons", "ixScoreboard", function(tabs)
+	tabs["scoreboard"] = function(container)
+		container:Add("ixScoreboard")
+	end
+end)
