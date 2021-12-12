b016c7efd75d6225e35469c1ff0567b96d7af869
Riggs <49407096+RiggsMackay@users.noreply.github.com>
2021-12-12 15:04:10 +0100

    Create sh_08_staffmember.lua

---
+++ schema/factions/sh_08_staffmember.lua
@@ -0,0 +1,31 @@
+--[[ Base Config ]]--
+
+FACTION.name = "Staff Member"
+FACTION.description = [[No need to put anything here..]]
+FACTION.color = Color(20, 200, 200)
+
+--[[ Helix Base Config ]]--
+
+FACTION.models = {
+	"models/player/kleiner.mdl",
+}
+
+FACTION.isGloballyRecognized = true
+FACTION.isDefault = true
+
+FACTION.payTime = 600
+FACTION.pay = 0
+
+--[[ Custom Config ]]--
+
+FACTION.defaultClass = nil
+FACTION.adminOnly = true
+FACTION.donatorOnly = false
+FACTION.noModelSelection = true
+FACTION.requiredXP = nil
+FACTION.command = nil
+FACTION.modelWhitelist = "player/kleiner"
+
+--[[ Do not change! ]]--
+
+FACTION_STAFF = FACTION.index
