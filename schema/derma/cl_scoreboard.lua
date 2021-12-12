93483fa9e44a8713736194d7f2ad79ba4d6ec064
Scotnay <maxandjude123@gmail.com>
2021-12-12 12:53:21 +0000

    Fix Scoreboard
    Fixed an error when a player leaves while the scoreboard is open and made it so that the scoreboard will reformat in the event of a player leaving.

--- schema/derma/cl_scoreboard.lua
+++ schema/derma/cl_scoreboard.lua
@@ -3,57 +3,87 @@ local PANEL = {}
 function PANEL:Init()
 	self:Dock(FILL)
 
-    local players = player.GetAll()
+	self.clients = { }
+end
+
+// Used so that the scoreboard can be refreshed and formatted in the event of
+// a player leaving.
+function PANEL:PopulateScoreboard()
+	for i, v in pairs( self.clients ) do
+		if ( ispanel( v ) and IsValid( v ) ) then
+			v:Remove()
+		end
+	end
+
+	// Clear the client panel cache.
+	self.clients = { }
+
+	local players = player.GetAll()
     table.sort(players, function(k, v) return k:Team() > v:Team() end)
 
-    for k, v in pairs(players) do
-        local playerBox = self:Add("DPanel")
-        playerBox:SetSize(self:GetWide(), 70)
-        playerBox:Dock(TOP)
-        playerBox:DockMargin(0, 0, 0, 5)
-        playerBox.Paint = function(this)
-            local teamColor = team.GetColor(v:Team()) or color_white
-            draw.RoundedBox(5, 0, 0, playerBox:GetWide(), playerBox:GetTall(), ColorAlpha(teamColor, 100))
+	for k, v in pairs(players) do
+		local playerBox = self:Add("DPanel")
+		self.clients[ k ] = playerBox
+		playerBox.player = v
+		playerBox:SetSize(self:GetWide(), 70)
+		playerBox:Dock(TOP)
+		playerBox:DockMargin(0, 0, 0, 5)
+		playerBox.Paint = function(this)
+			if (!IsValid( this.player ) ) then
+				return
+			end
 
-            draw.SimpleText(v:SteamName(), "LiteNetworkFont32", 80, playerBox:GetTall() / 2, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
-            draw.SimpleText(team.GetName(v:Team()), "LiteNetworkFont32", playerBox:GetWide() / 2, playerBox:GetTall() / 2, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+			local teamColor = team.GetColor(this.player:Team()) or color_white
+			draw.RoundedBox(5, 0, 0, playerBox:GetWide(), playerBox:GetTall(), ColorAlpha(teamColor, 100))
 
-            if ( LocalPlayer():IsAdmin() ) then
-                draw.SimpleText(v:Nick(), "LiteNetworkFont32", playerBox:GetWide() - 80, playerBox:GetTall() / 2, color_white, TEXT_ALIGN_RIGHT, TEXT_ALIGN_CENTER)
-            end
-        end
+			draw.SimpleText(v:SteamName(), "LiteNetworkFont32", 80, playerBox:GetTall() / 2, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+			draw.SimpleText(team.GetName(v:Team()), "LiteNetworkFont32", playerBox:GetWide() / 2, playerBox:GetTall() / 2, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 
-        local playerIcon = playerBox:Add("ixSpawnIcon")
-        playerIcon:SetSize(playerBox:GetTall() - 10, playerBox:GetTall() - 10)
-        playerIcon:Dock(LEFT)
-        playerIcon:DockMargin(5, 5, 5, 5)
-        playerIcon:SetModel(v:GetModel(), v:GetSkin())
-        playerIcon.DoRightClick = function()
-            local ply = self.player
-    
-            if (!IsValid(ply)) then
-                return
-            end
-    
-            local menu = DermaMenu()
-    
-            menu:AddOption(L("viewProfile"), function()
-                ply:ShowProfile()
-            end)
-    
-            menu:AddOption(L("copySteamID"), function()
-                SetClipboardText(ply:IsBot() and ply:EntIndex() or ply:SteamID())
-            end)
-    
-            hook.Run("PopulateScoreboardPlayerMenu", ply, menu)
-            menu:Open()
-        end
+			if ( LocalPlayer():IsAdmin() ) then
+				draw.SimpleText(v:Nick(), "LiteNetworkFont32", playerBox:GetWide() - 80, playerBox:GetTall() / 2, color_white, TEXT_ALIGN_RIGHT, TEXT_ALIGN_CENTER)
+			end
+		end
 
-        for k2, v2 in pairs(v:GetBodyGroups()) do
-            playerIcon.Entity:SetBodygroup(v2.id, v:GetBodygroup(v2.id))
-        end
-    end
+		playerBox.Think = function( this )
+			if ( !IsValid( this.player ) ) then
+				// Reformat if the player is invalid (left or bugged).
+				self:PopulateScoreboard()
+			end
+		end
+
+		local playerIcon = playerBox:Add("ixSpawnIcon")
+		playerIcon.player = v
+		playerIcon:SetSize(playerBox:GetTall() - 10, playerBox:GetTall() - 10)
+		playerIcon:Dock(LEFT)
+		playerIcon:DockMargin(5, 5, 5, 5)
+		playerIcon:SetModel(v:GetModel(), v:GetSkin())
+		playerIcon.DoRightClick = function()
+			local ply = self.player
+
+			if (!IsValid(ply)) then
+				return
+			end
+
+			local menu = DermaMenu()
+
+			menu:AddOption(L("viewProfile"), function()
+				ply:ShowProfile()
+			end)
+
+			menu:AddOption(L("copySteamID"), function()
+				SetClipboardText(ply:IsBot() and ply:EntIndex() or ply:SteamID())
+			end)
+
+			hook.Run("PopulateScoreboardPlayerMenu", ply, menu)
+			menu:Open()
+		end
+
+		for k2, v2 in pairs(v:GetBodyGroups()) do
+			playerIcon.Entity:SetBodygroup(v2.id, v:GetBodygroup(v2.id))
+		end
+	end
 end
+
 
 vgui.Register("ixScoreboard", PANEL, "DScrollPanel")
 
