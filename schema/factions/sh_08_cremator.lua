67cc691ab99a643aa68aa54c032a3f5466ee77af
RiggsMackay <xconnorx@gmx.de>
2021-12-25 00:55:56 +0100

    cream?

--- schema/factions/sh_08_cremator.lua
+++ schema/factions/sh_08_cremator.lua
@@ -1,8 +1,8 @@
 --[[ Base Config ]]--
 
-FACTION.name = "Staff Member"
-FACTION.description = [[No need to put anything here..]]
-FACTION.color = Color(20, 200, 200)
+FACTION.name = "Cremator"
+FACTION.description = [[No need to put anything here.. atm!]]
+FACTION.color = Color(15, 100, 15)
 
 --[[ Helix Base Config ]]--
 
@@ -18,15 +18,15 @@ FACTION.pay = 0
 
 --[[ Custom Config ]]--
 
-FACTION.voicelinesHuman = true
+FACTION.voicelinesHuman = false
 FACTION.defaultClass = nil
-FACTION.adminOnly = true
-FACTION.donatorOnly = false
+FACTION.adminOnly = false
+FACTION.donatorOnly = true
 FACTION.noModelSelection = true
-FACTION.requiredXP = nil
+FACTION.requiredXP = 5000
 FACTION.command = nil
 FACTION.modelWhitelist = "player/kleiner"
 
 --[[ Do not change! ]]--
 
-FACTION_STAFF = FACTION.index
+FACTION_CREMATOR = FACTION.index
