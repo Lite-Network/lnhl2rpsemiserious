7d35a54f7573c2478b9e9139b5fa6dac4e728ffe
RiggsMackay <xconnorx@gmx.de>
2021-12-26 18:56:11 +0100

    Create sh_bath_bomb.lua

---
+++ schema/items/sh_bath_bomb.lua
@@ -0,0 +1,15 @@
+-- Item Statistics
+
+ITEM.name = "Bath Bomb"
+ITEM.description = "Fuck you Alphenex."
+ITEM.category = "Items"
+
+-- Item Configuration
+
+ITEM.model = "models/maxofs2d/hover_classic.mdl"
+ITEM.skin = 0
+
+-- Item Inventory Size Configuration
+
+ITEM.width = 1
+ITEM.height = 1
