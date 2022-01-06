5efee1594e1f1ff897b12f527fba61bb5459559c
RiggsMackay <xconnorx@gmx.de>
2022-01-06 02:12:53 +0100

    Update Event Plugin

--- plugins/eventsystem/sv_plugin.lua
+++ plugins/eventsystem/sv_plugin.lua
@@ -1,17 +1,19 @@
 util.AddNetworkString("ixEventMessage")
 
-function PlayEventSound(sound)
+function PlayEventSound(sound, pitch)
 	for k, v in pairs(player.GetAll()) do
 		if istable(sound) then
-			v:PlaySound(table.Random(sound))
+			v:PlaySound(tostring(table.Random(sound)), tonumber(pitch) or 100)
 		else
-			v:PlaySound(sound)
+			v:PlaySound(tostring(sound), tonumber(pitch) or 100)
 		end
 	end
 end
 
-function PlayTimedEventSound(time, sound)
-	timer.Simple(time, function() PlayEventSound(tostring(sound)) end)
+function PlayTimedEventSound(time, sound, pitch)
+	timer.Simple(time, function()
+		PlayEventSound(tostring(sound), tonumber(pitch) or 100)
+	end)
 end
 
 function StopEventSound(sound)
@@ -24,6 +26,34 @@ function StopTimedEventSound(time, sound)
 	timer.Simple(time, function()
 		for k, v in pairs(player.GetAll()) do
 			v:StopSound(sound)
+		end
+	end)
+end
+
+function EmitShake()
+    for k, v in pairs(player.GetAll()) do
+        util.ScreenShake(v:GetPos(), 5, 5, 5, 5000)
+    end
+end
+
+function EmitTimedShake(time)
+	timer.Simple(time, function()
+		for k, v in pairs(player.GetAll()) do
+			util.ScreenShake(v:GetPos(), 5, 5, 5, 5000)
+		end
+	end)
+end
+
+function EmitSlightShake()
+    for k, v in pairs(player.GetAll()) do
+        util.ScreenShake(v:GetPos(), math.random(1, 3), 5, math.random(1, 3), 5000)
+    end
+end
+
+function EmitTimedSlightShake(time)
+	timer.Simple(time, function()
+		for k, v in pairs(player.GetAll()) do
+			util.ScreenShake(v:GetPos(), math.random(1, 3), 5, math.random(1, 3), 5000)
 		end
 	end)
 end
